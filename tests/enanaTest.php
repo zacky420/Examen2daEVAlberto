62c932f8b132896c971ebce401ccb439083a8730
zacky420 <albertoserrano7@hotmail.com>
2024-02-06 12:21:33 +0100

    OTRA VE

--- tests/enanaTest.php
+++ tests/enanaTest.php
@@ -7,22 +7,23 @@ class EnanaTest extends TestCase {
     
     public function testCreandoEnana() {
          // Crear enana viva
-        $enanaViva = new Enana("Enana1", 50);
-        $this->assertEquals("Enana1", $enanaViva->getNombre());
-        $this->assertEquals(50, $enanaViva->getPuntosVida());
-        $this->assertEquals("viva", $enanaViva->getSituacion());
+     // Crear enana viva
+$enanaViva = new Enana("Enana1", 50);
+$this->assertEquals("Enana1", $enanaViva->getNombre());
+$this->assertEquals(50, $enanaViva->getPuntosVida());
+$this->assertEquals("viva", $enanaViva->getSituacion());
 
-        // Crear enana muerta
-        $enanaMuerta = new Enana("Enana2", -10);
-        $this->assertEquals("Enana2", $enanaMuerta->getNombre());
-        $this->assertEquals(-10, $enanaMuerta->getPuntosVida());
-        $this->assertEquals("muerta", $enanaMuerta->getSituacion());
+// Crear enana muerta
+$enanaMuerta = new Enana("Enana2", -10);
+$this->assertEquals("Enana2", $enanaMuerta->getNombre());
+$this->assertEquals(-10, $enanaMuerta->getPuntosVida());
+$this->assertEquals("muerta", $enanaMuerta->getSituacion());
 
-        // Crear enana en limbo
-        $enanaLimbo = new Enana("Enana3", 0);
-        $this->assertEquals("Enana3", $enanaLimbo->getNombre());
-        $this->assertEquals(0, $enanaLimbo->getPuntosVida());
-        $this->assertEquals("limbo", $enanaLimbo->getSituacion());
+// Crear enana en limbo
+$enanaLimbo = new Enana("Enana3", 0);
+$this->assertEquals("Enana3", $enanaLimbo->getNombre());
+$this->assertEquals(0, $enanaLimbo->getPuntosVida());
+$this->assertEquals("limbo", $enanaLimbo->getSituacion());
     
     }
     public function testHeridaLeveVive() {
