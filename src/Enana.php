8e4e7324e19724694c83a49dcd8e7854e5dabaa7
zacky420 <albertoserrano7@hotmail.com>
2024-02-06 12:31:13 +0100

    probamos 3er test nuevamente con cambios en el código

--- src/Enana.php
+++ src/Enana.php
@@ -18,9 +18,13 @@ class Enana
 
     public function heridaLeve(){
         #Se le quitan 10 puntos de vida a la Enana y además se cambia el valor de situacion (si fuera necesario)
-        $this->puntosVida-=10;
+        $this->puntosVida -= 10;
+    
+        if ($this->puntosVida <= 0) {
+            $this->puntosVida = 0;
+            $this->situacion = "muerta";
+        }
     }
-
     public function heridaGrave(){
         #Se le quita toda la vida que posea hasta tener 0 puntos de vida y cambiarle la situacion a limbo
         $this->situacion="limbo";
